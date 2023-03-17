1468a02d605551e88deb0572a878d59803f59c45
Eman Mahfouz <emankahled614@gmail.com>
2023-03-17 02:47:15 -0700

    first code in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
-int main ()
+int main (void)
 {
-	char myString[] ="\"Programming is like building a multilingual puzzle";
-         puts(myString);	
+        puts("\"Programming is like building a multilingual puzzle");	
 	return 0;
 }
