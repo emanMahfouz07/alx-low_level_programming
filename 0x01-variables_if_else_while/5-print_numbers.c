66dbaeb5ab7f2100b91395ceab128ec4dd3c9b1f
Eman Mahfouz <emankahled614@gmail.com>
2023-03-19 07:45:19 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
+ */
+int main(void) {
+    int n;
+    for (int n = 0; i <1; i++)
+        printf("%d", i);
+    printf("\n");
+    return 0;
+}
