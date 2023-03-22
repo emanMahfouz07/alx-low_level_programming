181ca31564de534d341e0982574a64f4a3facab3
Eman Mahfouz <emankahled614@gmail.com>
2023-03-22 06:50:21 -0700

    2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+	char i;
+
+	while (n < 10)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		n++;
+		_putchar("\n");
+	}
