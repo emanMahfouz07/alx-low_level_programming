37afe3a7860f3fba90e8b848362ab2bf18672784
Eman Mahfouz <emankahled614@gmail.com>
2023-03-31 14:06:16 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,29 @@
 */
 char *cap_string(char *str)
 {
-	int index = 0;
+	int i, j;
+	char sep[50] = {' ', '\n', '\t', ',', ';', '.', '!', '?', '"', '(', ')',
+			 '{', '}' };
 
-	while (str[++index])
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||
-				str[index - 1] == '\n' ||
-				str[index - 1] == ',' ||
-				str[index - 1] == ';' ||
-				str[index - 1] == '.' ||
-				str[index - 1] == '!' ||
-				str[index - 1] == '?' ||
-				str[index - 1] == '"' ||
-				str[index - 1] == '(' ||
-				str[index - 1] == ')' ||
-				str[index - 1] == '{' ||
-				str[index - 1] == '}')
-			str[index] -= 32;
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (i == 0)
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+				{
+					str[i] = str[i] - 32;
+				}
+			}
+			if (str[i] == sep[j])
+			{
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
+			}
+		}
 	}
 	return (str);
 }
