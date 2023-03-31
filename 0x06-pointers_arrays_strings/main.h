f0cb7f59232c788778030c50a1fe326681dfbc75
Eman Mahfouz <emankahled614@gmail.com>
2023-03-31 06:21:47 -0700

    0

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+#include <stdio.h>
+/**
+ * char prototypes()
+ * int prototypes()
+ * void prototypes()
+ */
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *string_toupper(char *);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *cap_string(char *);
+char *leet(char *);
+#endif /*main_h*/
