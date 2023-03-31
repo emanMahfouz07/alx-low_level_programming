10209c661ea77b6d18554e5328920c8d3c7f9bb1
Eman Mahfouz <emankahled614@gmail.com>
2023-03-31 13:06:55 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,6 @@
 #include <main.h>
 /**
- * _strcat - concatenates  the string pointed to by @src to
+ * _struct - concatenates  the string pointed to by @src to
  * the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
