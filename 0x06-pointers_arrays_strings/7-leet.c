8c69452891924197f65f2fb22f57a1c20db9d79a
Eman Mahfouz <emankahled614@gmail.com>
2023-03-31 14:16:42 -0700

    7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,19 @@
 
 char *leet(char *str)
 {
-	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
+	int i, j;
 
-	while (str[++index1])
+	char alpha[80] = "aAeEoOtTlL";
+	char numbers[80] = "43071";
+
+	for (i = 0; str[i] != '\0'; ++i)
 	{
-		for (index2 = 0; index2 <= 7; index2++)
+		for (j = 0; alpha[j] != '\0'; j++)
 		{
-			if (str[index1] == leet[index2] ||
-			 str[index1] - 32 == leet[index2])
-				str[index1] = index2 + '0';
+			if (str[i] == alpha[j])
+			{
+				str[i] = numbers[j / 2];
+			}
 		}
 	}
 	return (str);
