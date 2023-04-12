6e3f387dea1627d377d4358e1d19bbeb475705ec
Eman Mahfouz <emankahled614@gmail.com>
2023-04-11 18:48:11 -0700

    array of chars

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,11 +14,12 @@ char *create_array(unsigned int size ,char c)
 		return (NULL);
 
 	char *arr;
+	unsigned int i;
 
 	arr = malloc(sizeof(char) * size);
 	if (arr == NULL)
 		return (NULL);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		arr[i] = c;
 	
 	return (arr);
