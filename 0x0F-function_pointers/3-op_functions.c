04b577ce5e6470c6f02a2df64cabaa1e98beeb34
Eman Mahfouz <emankahled614@gmail.com>
2023-04-19 17:21:33 -0700

    ....

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include "3-calc.h"
+
+/**
+ * op_add - return the sum of two numbers
+ * @a:first number
+ * @b:second number
+ *
+ * Return:add of two numbers
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - return the subtraction of two numbers
+ * @a:first number
+ * @b:second number
+ *
+ * Return:subtract of two numbers
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - return the multipliction of two numbers
+ * @a:first number
+ * @b:second number
+ *
+ * Return:multiplication of two numbers
+ */
+
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - return the division of two numbers
+ * @a:first number
+ * @b:second number
+ *
+ * Return:division of two numbers
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - return the reminder of two numbers
+ * @a:first number
+ * @b:second number
+ *
+ * Return:reminder of two numbers
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x0F-function_pointers/main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always 0.
+ */
+
+int main (
