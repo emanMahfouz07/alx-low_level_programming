a0867f6fc79ac5fb4de6f50afed6bcc5e5535ee0
Eman Mahfouz <emanKahled614@gmail.com>
2023-04-30 15:59:26 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,8 @@
  * Return: number of args
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	void (argv);
 	printf("%d\n", argc - 1);
 
 	return (0);
