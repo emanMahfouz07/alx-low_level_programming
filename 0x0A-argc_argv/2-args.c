8785c916d0100fd2e13f573a7b46842b163d91f2
Eman Mahfouz <emanKahled614@gmail.com>
2023-04-30 16:11:55 -0700

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ * main - prints all arguments it recives
+ * @argc: argument count
+ * @argv: argument values
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+
+	for (index = 0; index < argc; index++)
+		printf("%s\n", argv[index]);
+
+	return (0);
+}
