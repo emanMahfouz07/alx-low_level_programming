0a9350a052f392984fb22133ebdf360a3838a166
Eman Mahfouz <emanKahled614@gmail.com>
2023-04-30 16:28:50 -0700

    0

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
- * main - print result of multiplication
- * of two numbers
- * @argc:number of arguments
- * @argv: value to this argument
- * Return: return 0
+/**
+ * main - print result of multiplication of two numbers
+ *@argc:number of arguments
+ *@argv: value to this argument
+ *Return: return 0
  */
 
 int main(int argc, char *argv[])
