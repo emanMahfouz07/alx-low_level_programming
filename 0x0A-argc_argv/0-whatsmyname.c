1ed2e8eae0d79503b2a89c6b7470c643c5c948a7
Eman Mahfouz <emanKahled614@gmail.com>
2023-04-30 15:41:23 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main - prints program name
+ * argc: number of argumernts
+ * argv:array of values of the arguments
+ * Return:0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
