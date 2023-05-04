db3ff113611ddef675bada9451ec00f5ddf54e46
Eman Mahfouz <emanKahled614@gmail.com>
2023-05-04 14:30:45 -0700

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: the binary number as a string
+ *
+ * Return: the converted value
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal = 0;
+
+	if (b == NULL)
+		return (0);
+	int i = 0;
+	for (b[i] != '\0'; i++)
+	{
+		if (b[i] == '0')
+			decimal <<= 1;
+		else if (b[i] == '1')
+			decimal = (decimal << 1) | 1;
+		else
+			return (0);
+	}
+	return (decimal);
+}
