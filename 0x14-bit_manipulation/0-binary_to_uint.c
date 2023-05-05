865f13585b8bb67adf34aa64b0cd2278047a7176
Eman Mahfouz <emanKahled614@gmail.com>
2023-05-05 08:52:26 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
+	unsigned int output = 0;
+	int str_len = 0, base = 1;
 
+	while (b[str_len] != '\0')
+		str_len++;
 	if (b == NULL)
 		return (0);
-	unsigned int i;
-
-	for (i = 0; b[i] != '\0'; i++)
+	while (*b)
 	{
-		if (b[i] == '0')
-			decimal <<= 1;
-		else if (b[i] == '1')
-			decimal = (decimal << 1) | 1;
-		else
+		if (*b != '1' && *b != '0')
 			return (0);
+		b++;
 	}
-	return (decimal);
+	while (str_len)
+	{
+		output += ((b[str_len - 1] - '0') * base);
+		base *= 2;
+		str_len--;
+	}
+	return (output);
 }
